c68b9710d13849fae18bfbb522b4a63f3b887296
lrnd.__ <68582209+Tamvan15@users.noreply.github.com>
2024-10-09 17:18:04 +0700

    ngentot

--- core/urls.py
+++ core/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('managemen/', include('managemen.urls')),  # Tambahkan baris ini
-    path('', admin.site.urls, name='admin_login'),
+    path('managemen/', include('managemen.urls')),
+    path('', RedirectView.as_view(url='/admin/'), name='home'),  # Redirect root ke admin
 ]
 
 if settings.DEBUG:
--- managemen/urls.py
+++ managemen/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .admin import ProyekAdmin
 from django.contrib import admin
 
+app_name = 'managemen'  # Tambahkan ini untuk memberikan namespace unik
+
 urlpatterns = [
     path('proyek/<int:proyek_id>/print/', ProyekAdmin.print_proyek, name='print_proyek'),
     path('proyek/print_all/', ProyekAdmin.print_all_proyek, name='print_all_proyek'),
